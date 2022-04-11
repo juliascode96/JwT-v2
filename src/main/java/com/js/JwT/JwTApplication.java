82f8f667b6fdb74d6ba91e99d120996b749f80a1
Julia Scodelari <j.scodelari96@gmail.com>
2022-04-11 19:52:53 -0300

    primer commit

---
+++ src/main/java/com/js/JwT/JwTApplication.java
@@ -0,0 +1,13 @@
+package com.js.JwT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/js/JwT/JwTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.JwT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
